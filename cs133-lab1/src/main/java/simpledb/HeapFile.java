b3e1f8bc056a677100cb69cc191a19f936588719
caathead <155051876@qq.com>
2024-05-28 10:47:00 +0800

    通过了 HeapFileReadTest个单元测试 完成HeapFile的构建 主要集中于readpage 和iterator的构建 同时规定了Buffpool的初始化在迭代器中进行

--- cs133-lab1/src/main/java/simpledb/HeapFile.java
+++ cs133-lab1/src/main/java/simpledb/HeapFile.java
@@ -71,8 +71,19 @@ public class HeapFile implements DbFile {
     // see DbFile.java for javadocs
     public Page readPage(PageId pid) {
         // some code goes here
+        Page page = null;
+        byte[] data = new byte[BufferPool.getPageSize()];
 
-        return null;
+        try (RandomAccessFile raf = new RandomAccessFile(getFile(), "r")) {
+            // page在HeapFile的偏移量
+            int pos = pid.getPageNumber() * BufferPool.getPageSize();
+            raf.seek(pos);
+            raf.read(data, 0, data.length);
+            page = new HeapPage((HeapPageId) pid, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return page;
     }
 
     // see DbFile.java for javadocs
@@ -86,7 +97,8 @@ public class HeapFile implements DbFile {
      */
     public int numPages() {
         // some code goes here
-        return 0;
+
+        return (int) (f.length()/BufferPool.getPageSize());
     }
 
     // see DbFile.java for javadocs
@@ -108,7 +120,69 @@ public class HeapFile implements DbFile {
     // see DbFile.java for javadocs
     public DbFileIterator iterator(TransactionId tid) {
         // some code goes here
-        return null;
+        return new HeapFileIterator(tid);
+    }
+    private class HeapFileIterator implements DbFileIterator{
+        private int PagePos;  //逻辑应该是 遍历不同的页数 再访问不同页数中的tuple
+
+        private TransactionId tid;
+
+        private HeapPage Page;
+
+        private Iterator<Tuple> tupleIterator;
+        public HeapFileIterator(TransactionId tid){
+            this.tid=tid;
+        }
+        @Override
+        public void open() throws DbException, TransactionAbortedException {
+            PagePos=0;
+            HeapPageId heapPageId=new HeapPageId(getId(),PagePos);
+            //加载第一页的tuples
+            Page= (HeapPage) Database.getBufferPool().getPage(tid,heapPageId,Permissions.READ_ONLY);
+            tupleIterator=Page.iterator();
+        }
+
+        @Override
+        public boolean hasNext() throws DbException, TransactionAbortedException {
+            if(Page==null){  //判断开始页是否为空
+                throw new IllegalStateException("没有初始化或者初始页面为空");
+            }
+            if(tupleIterator.hasNext()){
+               return true;
+            }
+            //程序运行到这里 表明这一页已经读完了 往后一页走
+            if(PagePos < numPages()-1){
+                PagePos++;
+                HeapPageId heapPageId=new HeapPageId(getId(),PagePos);
+                Page= (HeapPage) Database.getBufferPool().getPage(tid,heapPageId,Permissions.READ_ONLY); //得到后一页
+                tupleIterator=Page.iterator();   //更新迭代器为后一页的迭代器
+                return tupleIterator.hasNext();                                    //但是别着急 先看看这一页有没有值
+            }else{ //表明没有下一页了
+                return false;
+            }
+        }
+
+        @Override
+        public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+            if(!hasNext()){
+                throw new IllegalStateException("该页无元素或者已经遍历完了");
+            }
+            return tupleIterator.next();
+        }
+
+        @Override
+        public void rewind() throws DbException, TransactionAbortedException {
+            //重置回到第一页
+            open();
+        }
+
+        @Override
+        public void close() {
+            //清空状态 一切重置
+            PagePos=0;
+            Page=null;
+            tupleIterator=null;
+        }
     }
 
 }
